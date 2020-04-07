184cf489dcb635302cf2d9c044dbc13cf5a26812
Mark <m.e.giles@gmail.com>
2020-04-06 20:13:59 -0700

    gettin it

--- modules/nav.php
+++ modules/nav.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top flex-md-nowrap"> <a class="navbar-brand" href="../index.html"><img src="../img/dluxcolorlogo.png" alt="dlux-logo" width="32" height="32" class="rounded"></a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top flex-md-nowrap"> <a class="navbar-brand" href="../"><img src="../img/dluxcolorlogo.png" alt="dlux-logo" width="32" height="32" class="rounded"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active"> <a class="nav-link" href="#">Apps<span class="sr-only">(current)</span></a></li>
-        <li class="nav-item"> <a class="nav-link" href="../dex/index.html">Exchange</a></li>
-		<li class="nav-item"> <a class="nav-link" href="../news/index.html">Updates</a></li>
-        <li class="nav-item"> <a class="nav-link" href="../docs/index.html">Documentation</a></li>
+        <li class="nav-item"> <a class="nav-link" href="../apps/">Apps</a></li>
+        <li class="nav-item"> <a class="nav-link" href="../dex/">Exchange</a></li>
+		<li class="nav-item"> <a class="nav-link" href="../news/">Updates</a></li>
+        <li class="nav-item"> <a class="nav-link" href="../docs/">Documentation</a></li>
       </ul>
       <ul class="navbar-nav ">
         <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" data-toggle="dropdown" href="#"><img src="../img/user-icon.svg" alt="Placeholder image" width="30" height="30" class="img-fluid rounded-circle bg-light mr-2">username</a>
