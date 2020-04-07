39020fed63904132b504d25263db80ada4ebec35
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-07 18:34:55 -0500

    remove session

--- modules/nav.php
+++ modules/nav.php
@@ -1,6 +1,3 @@
-<?php 
-    session_start();   
-?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top flex-md-nowrap"> <a class="navbar-brand" href="../"><img src="../img/dluxcolorlogo.png" alt="dlux-logo" width="32" height="32" class="rounded"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -10,9 +7,10 @@
 		<li class="nav-item"> <a class="nav-link" href="../news/">Updates</a></li>
         <li class="nav-item"> <a class="nav-link" href="../docs/">Documentation</a></li>
       </ul>
-<?php 
-
-if ($_SESSION['user']){ ?>
+	<ul class="navbar-nav" id="no-session">
+	<li class="nav-item"><a class="nav-link acct-link" href="../login/">Login</a></li>
+	<li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Sign up</a></li>
+	</ul>
     <ul class="navbar-nav" id="active-session">
         <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" data-toggle="dropdown" href="#"><img src="../img/user-icon.svg" alt="Placeholder image" width="30" height="30" class="img-fluid rounded-circle bg-light mr-2">'.$_SESSION['user'].'</a>
           <div class="dropdown-menu" aria-labelledby="userDropdown"> <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Profile</a> <a class="dropdown-item" href="#"><i class="fas fa-wallet mr-2"></i>Wallet</a> <a class="dropdown-item" href="../dashboard/index.html"><i class="fas fa-tools mr-2"></i>Tools</a> <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Settings</a>
@@ -22,13 +20,5 @@ if ($_SESSION['user']){ ?>
             <a class="dropdown-item" href="#"><i class="fas fa-user-friends mr-2"></i>Switch User</a> <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a></div>
         </li>
       </ul>
-
-<?php } else { ?>
-    <ul class="navbar-nav" id="no-session">
-		<li class="nav-item"><a class="nav-link acct-link" href="../login/">Login</a></li>
-		<li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Sign up</a></li>
-		</ul>
-<?php }
-?>	
     </div>
   </nav>
