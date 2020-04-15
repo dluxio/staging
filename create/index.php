abcd046c225df1b29d21669edd0055aeabae473e
Mark <m.e.giles@gmail.com>
2020-04-15 01:25:07 -0700

    fav

--- create/index.php
+++ create/index.php
@@ -3,6 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
 <title>DLUX - Create</title>
 <link href="../css/bootstrap-4.4.1.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
