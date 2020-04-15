6936fd9cf10b53b76e83130ade5b295c0f886676
Mark <m.e.giles@gmail.com>
2020-04-15 01:18:56 -0700

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -12,11 +12,13 @@
 	<li class="nav-item"><a class="nav-link acct-link" href="../login/">Login</a></li>
 	<li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Sign up</a></li>
 	</ul>
-    <div id="active-session">
-		<ul><li class="nav-item"><a href="../create/"><i class="fas fa-plus-circle mr-2"></i></a></li></ul>
-        <ul class="nav navbar-nav">
-		<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" data-toggle="dropdown" href="#"><img src="https://token.dlux.io/getauthorpic/{{author}}" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light mr-2 cover">
-		<span id="userName">username</span></a>
+    <div class="navbar-right" id="active-session">
+		<ul class="nav navbar-nav">
+		<li class="nav-item"><a href="../create/"><i class="fas fa-plus-circle mr-2"></i></a></li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" data-toggle="dropdown" href="#">
+			  <img src="https://token.dlux.io/getauthorpic/{{author}}" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light mr-2 cover">
+			  <span id="userName">username</span></a>
           <div class="dropdown-menu" aria-labelledby="userDropdown"> <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Profile</a> <a class="dropdown-item" href="#"><i class="fas fa-wallet mr-2"></i>Wallet</a><a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Settings</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#"><i class="fas fa-robot mr-2"></i>Nodes</a> <a class="dropdown-item" href="#"><i class="fas fa-info-circle mr-2"></i>About</a>
