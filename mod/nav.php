01f40fe0f77357b33e43e4297878734780a18e80
Mark <m.e.giles@gmail.com>
2020-04-17 13:51:05 -0700

    Update nav.php

--- mod/nav.php
+++ mod/nav.php
@@ -24,10 +24,10 @@
 			  <span id="userName">username</span></a>
           <div class="dropdown-menu" aria-labelledby="userDropdown">
 			 <a class="dropdown-item" href="../me/"><i class="fas fa-user fa-fw mr-2"></i>Profile</a>
-			 <a class="dropdown-item" href="../me/index.php#wallet"><i class="fas fa-wallet fa-fw mr-2"></i>Wallet</a>
-			 <a class="dropdown-item" href="../me/index.php#wallet"><i class="fas fa-cog fa-fw mr-2"></i>Settings</a>
+			 <a class="dropdown-item" href="../me/#wallet"><i class="fas fa-wallet fa-fw mr-2"></i>Wallet</a>
+			 <a class="dropdown-item" href="../me/index.php#node"><i class="fas fa-robot fa-fw mr-2"></i>Node</a>
+			 <a class="dropdown-item" href="../me/index.php#settings"><i class="fas fa-cog fa-fw mr-2"></i>Settings</a>
              <div class="dropdown-divider"></div>
-             <a class="dropdown-item" href="#"><i class="fas fa-robot fa-fw mr-2"></i>Nodes</a>
 			 <a class="dropdown-item" href="#"><i class="fas fa-info-circle fa-fw mr-2"></i>About</a>
              <div class="dropdown-divider"></div>
              <a class="dropdown-item" href="../login/"><i class="fas fa-user-friends mr-2"></i>Switch User</a>
