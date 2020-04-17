0bbe1d23d73cfdf1eaedc965115b722ac25fbd4f
Mark <m.e.giles@gmail.com>
2020-04-16 17:54:06 -0700

    Update nav.php

--- mod/nav.php
+++ mod/nav.php
@@ -16,8 +16,8 @@
     <div class="mr-5" id="active-session">
 	  <ul class="nav navbar-nav">
 		<li class="nav-item">
-			<a class="nav-link" href="../new/">
-				<i class="fas fa-fw fa-lg fa-plus-circle my-auto mr-2"></i></a></li>
+			<a class="nav-link my-auto" href="../new/">
+				<i class="fas fa-fw fa-lg fa-plus-circle mr-2"></i></a></li>
 		<li class="nav-item dropdown">
 		  <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" data-toggle="dropdown" href="#">
 			  <img src="https://token.dlux.io/getauthorpic/{{author}}" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light mr-2 cover">
