bd59ca9266409adac0cd8d8c5e8000b950c91c8d
Mark <m.e.giles@gmail.com>
2020-04-18 01:03:07 -0700

    Update nav.php

--- mod/nav.php
+++ mod/nav.php
@@ -24,7 +24,7 @@
 			  <span id="userName">username</span></a>
           <div class="dropdown-menu" aria-labelledby="userDropdown">
 			
-			 <a class="dropdown-item" href="../me/#blog"><i class="fas fa-user fa-fw mr-2" onClick="changeTab('blog')"></i>Profile</a>
+			 <a class="dropdown-item" href="../me/#blog" onClick="changeTab('blog')"><i class="fas fa-user fa-fw mr-2"></i>Profile</a>
 			 <a class="dropdown-item" href="../me/#wallet" onClick="changeTab('wallet')"><i class="fas fa-wallet fa-fw mr-2"></i>Wallet</a>
 			 <a class="dropdown-item" href="../me/#inventory" onClick="changeTab('inventory')"><i class="fas fa-boxes fa-fw mr-2"></i>Inventory</a>
 			 <a class="dropdown-item" href="../me/#node" onClick="changeTab('node')"><i class="fas fa-robot fa-fw mr-2"></i>Node</a>
