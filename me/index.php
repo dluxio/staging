a44b66833e9c66da1d9a49fd96ef64ad6eea3789
Mark <m.e.giles@gmail.com>
2020-04-18 00:37:28 -0700

    Update index.php

--- me/index.php
+++ me/index.php
@@ -586,17 +586,7 @@
 </main>
 <?php include '../mod/footer.php';?>
 <script>
-       // Javascript to enable link to tab
-var url = document.location.toString();
-if (url.match('#')) {
-    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-} 
 
-// Change hash for page-reload
-$('.nav-tabs a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-    window.scrollTo(0, 0);
-})
 	function updateVoteSubmit(id,val) {
           document.getElementById(id).innerHTML = document.getElementById(val).value + '%'; 
         }</script>
