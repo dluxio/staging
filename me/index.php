034b4e9ebe7c33d7c5efbbd50289c6a42420a29c
Mark <m.e.giles@gmail.com>
2020-04-17 17:43:54 -0700

    Update index.php

--- me/index.php
+++ me/index.php
@@ -571,8 +571,7 @@
 	// Javascript to enable link to tab
 var url = document.location.toString();
 if (url.match('#')) {
-    if(url.match('me/index.php')){$('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');}
-    else {$('.nav-tabs a[href="/me/#' + url.split('#')[1] + '"]').tab('show');}
+    $('.nav-tabs a[href="me/#' + url.split('#')[1] + '"]').tab('show');
 } 
 
 // Change hash for page-reload
