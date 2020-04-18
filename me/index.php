7deadec6e8254b9fb0c426411524567779a54ce9
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-18 12:14:45 -0500

    meh

--- me/index.php
+++ me/index.php
@@ -646,10 +646,9 @@
 // Javascript to enable link to tab
 function pageSpecfic(usr){
 	document.getElementById('dluxamountlab').innerHTML = `Amount (Balance <a href="#" onClick="insertBal()">${parseFloat(parseInt(usr.dlux.balance)/1000).toFixed(3)} DLUX</a>):`
-	var dluxEl =document.getElementById('dluxactions').firstElementChild.innerHtml = `${parseFloat(parseInt(usr.dlux.balance)/1000).toFixed(3)} DLUX`
-	var dluxpEl = document.getElementById('dluxpactions').firstElementChild.innerHtml = `${parseFloat(parseInt(usr.dlux.poweredUp)/1000).toFixed(3)} DLUX`
-	console.log(dluxEl,dluxpEl)
-	}	
+	document.getElementById('dluxactions').firstElementChild.innerHtml = `${parseFloat(parseInt(usr.dlux.balance)/1000).toFixed(3)} DLUX`
+	document.getElementById('dluxpactions').firstElementChild.innerHtml = `${parseFloat(parseInt(usr.dlux.poweredUp)/1000).toFixed(3)} DLUX`
+		}	
 	
 var url = document.location.toString();
 if (url.match('#')) {
