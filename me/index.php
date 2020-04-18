03b835e7789f6f6169ad9d2d233955b8a8d9c86e
Mark <m.e.giles@gmail.com>
2020-04-17 17:50:03 -0700

    ugly

--- me/index.php
+++ me/index.php
@@ -571,7 +571,7 @@
 	// Javascript to enable link to tab
 var url = document.location.toString();
 if (url.match('#')) {
-    $('.nav-tabs a[href="index.php#' + url.split('index.php#')[1] + '"]').tab('show');
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
 } 
 
 // Change hash for page-reload
