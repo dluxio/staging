7c652a25861e10b988b491ad7e8a91c4e7c4fb51
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-20 08:19:23 -0500

    four

--- js/me.js
+++ js/me.js
@@ -1,21 +1,26 @@
 function me(usr) {
     usr.hbd = { balance: usr.hive.sbd_balance }
-    console.log('here?')
     User.hbd = { balance: usr.hive.sbd_balance }
-    console.log(usr.hive.balance)
     document.getElementById('hiveactions').firstElementChild.innerText = usr.hive.balance
     document.getElementById('hbdactions').firstElementChild.innerText = usr.hive.sbd_balance
-    console.log('this')
     document.getElementById('savingsactions').firstElementChild.innerText = User.hive.savings_sbd_balance
-    console.log('what')
+
     document.getElementById('powerdluxamountlab').innerHTML = `Amount (Balance <a href="#" onClick="insertBal(parseFloat(parseInt(User.dlux.balance)/1000),'powerupdluxamount')">${parseFloat(parseInt(usr.dlux.balance)/1000).toFixed(3)} DLUX</a>):`
+    console.log('here?1')
     document.getElementById('powerdowndluxammount').innerHTML = `Amount (Balance <a href="#" onClick="insertBal(User.dlux.poweredUp,'powerupdluxamount')">${parseFloat(usr.dlux.poweredUp/1000).toFixed(3)} DLUX</a>):`
+    console.log('here?')
     document.getElementById('senddluxammount').innerHTML = `Amount (Balance <a href="#" onClick="insertBal(User.dlux.balance,'senddluxamount')">${parseFloat(usr.dlux.balanace/1000).toFixed(3)} DLUX</a>):`
+    console.log('here?3')
     document.getElementById('dluxactions').firstElementChild.innerText = `${parseFloat(parseInt(usr.dlux.balance)/1000).toFixed(3)} DLUX`
+    console.log('here?4')
     document.getElementById('dluxpactions').firstElementChild.innerText = `${parseFloat(parseInt(usr.dlux.poweredUp)/1000).toFixed(3)} DLUX`
+    console.log('here?5')
     document.getElementById('hivepactions').firstElementChild.innerText = parseFloat((parseFloat(usr.hstats.total_vesting_fund_steem) * parseFloat(usr.hive.vesting_shares)) / parseFloat(usr.hstats.total_vesting_shares)).toFixed(3) + ' HP'
+    console.log('here?6')
     document.getElementById('hiveval').firstElementChild.innerText = `$${parseFloat((parseFloat(( parseFloat(usr.hstats.total_vesting_fund_steem) * parseFloat(usr.hive.vesting_shares)) / parseFloat(usr.hstats.total_vesting_shares)) + parseFloat(usr.hive.balance))*usr.price).toFixed(2)}`
+    console.log('here?7')
     document.getElementById('dluxval').firstElementChild.innerText = `$${parseFloat(((parseInt(usr.dlux.balance) + parseInt(usr.dlux.poweredUp))/1000)*parseFloat(usr.dex.markets.hive.tick)*parseFloat(usr.price)).toFixed(2)}`
+    console.log('here?8')
     document.getElementById('buylink').addEventListener("click", function() {
         User.opts.type = 'buy'
         dexmodal("hive", "buy");
