ad4118941dcddd7f58705cbaec8f3be92846540d
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-20 01:44:53 -0500

    please

--- js/me.js
+++ js/me.js
@@ -42,7 +42,7 @@ function dexmodal(pair,type){
 		}
 		for(i in User.dex.queue){
 			if(User.opts.to !== User.dex.queue[i]){
-				console.log(document.getElementById('escrowAgent'))
+				console.log(document.getElementById('escrowAgent').innerText)
 				if(document.getElementById('escrowAgent').innerText == 'Escrow Agent '){document.getElementById('escrowAgent').innerText == User.dex.queue[i]}
 				var node = document.createElement('li')
 				node.innerHTML = `<a href="#">${User.dex.queue[i]} - Fee: .1DLUX - Trust: 9 - Liquid: 1000000000</a>`
@@ -51,6 +51,7 @@ function dexmodal(pair,type){
 		}
 		for(i in User.dex.queue){
 			if(User.opts.agent !== User.dex.queue[i]){
+				console.log(document.getElementById('custodialAgent').innerText)
 				if(document.getElementById('custodialAgent').innerText == 'Custodial Agent ' && document.getElementById('escrowAgent').innerText != User.dex.queue[i]){document.getElementById('custodialAgent').innerText == User.dex.queue[i]}
 				var node = document.createElement('li')
 				node.innerHTML = `<a href="#">${User.dex.queue[i]} - Fee: .1DLUX - Trust: 9 - Liquid: 1000000000</a>`
